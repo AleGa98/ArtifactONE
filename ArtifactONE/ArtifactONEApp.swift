79162a4819d73f694a03f739ca9bc83627245084
AleGa98 <gll.ale998@gmail.com>
2022-03-25 14:52:54 +0100

    Start

---
+++ ArtifactONE/ArtifactONEApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArtifactONEApp.swift
+//  ArtifactONE
+//
+//  Created by Alessandro Gallo on 25/03/22.
+//
+
+import SwiftUI
+
+@main
+struct ArtifactONEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
